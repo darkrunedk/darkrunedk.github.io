f840bf2e46759986654c5a8acad4c5191fdbe805
Tobias Belling Christensen <programmering@darkrune.dk>
2017-10-29 07:44:34 +0100

    More testing...

--- js/core.js
+++ js/core.js
@@ -2,12 +2,9 @@ $(document).ready(function() {
   $.ajax({
     url: "https://api.github.com/users/darkrunedk/repos"
   }).done(function(data) {
-    console.log(data);
-  });
-
-  $.ajax({
-    url: "https://api.github.com/users/darkrunedk"
-  }).done(function(data) {
-    console.log(data);
+    data.each(function(k,v) {
+      console.log(k, v);
+    });
+    //$("#projects").append()
   });
 });
