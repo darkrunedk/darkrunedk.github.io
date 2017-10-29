4457a511eb43e0129dd09ef13c7077ac08fb523e
Tobias Belling Christensen <programmering@darkrune.dk>
2017-10-29 08:19:06 +0100

    Added a little bit extra

--- js/core.js
+++ js/core.js
@@ -1,9 +1,27 @@
+var blacklist = ["gray_owl"];
+
 $(document).ready(function() {
   $.ajax({
     url: "https://api.github.com/users/darkrunedk/repos"
   }).done(function(data) {
     for (var i = 0; i < data.length; i++) {
-      console.log("Number " + i + ": " + data[i]);
+      var obj = data[i];
+
+      var blacklisted = ($.inArray(obj.name, blacklist) > -1) ? true : false;
+      if (!blacklisted) {
+        var div = $("<div id='" + obj.name.toLowerCase() + "'>");
+        div.append("<h3><a href='" + obj.html_url + "' target='_blank'>" + obj.name + "</a></h3>");
+        div.append("<p>" + obj.description + "</p>");
+
+        if (obj.homepage) {
+          div.append("<p><a href='" + obj.homepage + "' target='_blank'>Homepage</a></p>");
+        }
+
+        $("#project-container").append(div);
+      } else {
+        continue;
+      }
+      //console.log(div);
     }
     //$("#projects").append()
   });
