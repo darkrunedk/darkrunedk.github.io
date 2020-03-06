71189c244a9742b9b299d050f26187bae7c19b47
Tobias Belling Christensen <github@darkrune.dk>
2020-03-06 22:27:44 +0100

    Updated webpage
    Updated with user info, some styling etc.

--- js/core.js
+++ js/core.js
@@ -1,28 +1,45 @@
-var blacklist = ["gray_owl"];
+var blacklist = ["gray_owl", "darkrunedk.github.io", "Monosolutions"];
+var githubBio = $("#githubBio");
 
 $(document).ready(function() {
-  $.ajax({
-    url: "https://api.github.com/users/darkrunedk/repos"
-  }).done(function(data) {
-    for (var i = 0; i < data.length; i++) {
-      var obj = data[i];
-
-      var blacklisted = ($.inArray(obj.name, blacklist) > -1) ? true : false;
-      if (!blacklisted) {
-        var div = $("<div id='" + obj.name.toLowerCase() + "'>");
-        div.append("<h3><a href='" + obj.html_url + "' target='_blank'>" + obj.name + "</a></h3>");
-        div.append("<p>" + obj.description + "</p>");
-
-        if (obj.homepage) {
-          div.append("<p><a href='" + obj.homepage + "' target='_blank'>Homepage</a></p>");
-        }
-
-        $("#project-container").append(div);
-      } else {
-        continue;
-      }
-      //console.log(div);
-    }
-    //$("#projects").append()
-  });
+	
+	$(".js-anchor").on("click", function() {
+		let target = this.dataset.anchor;
+		$("html,body").animate({
+			scrollTop: $(target).offset().top
+		}, 700);
+	});
+	
+	$.ajax({
+		url: "https://api.github.com/users/darkrunedk"
+	}).done(function(data) {
+		$("#devname").text(data.name);
+		githubBio.append("<h1>" + data.name + "</h1>");
+		githubBio.append("<p>" + data.bio + "</p>");
+		githubBio.append("<p><a href='" + data.blog + "' target='_blank'>My website</a></p>");
+	});
+	
+	$.ajax({
+		url: "https://api.github.com/users/darkrunedk/repos"
+	}).done(function(data) {
+		data.forEach(function(project) {
+			var blacklisted = ($.inArray(project.name, blacklist) > -1) ? true : false;
+			if (!blacklisted) {
+				var div = $("<div id='" + project.name.toLowerCase() + "'>");
+				div.append("<h3><a href='" + project.html_url + "' target='_blank'>" + project.name.replace("-", " ") + "</a></h3>");
+				div.append("<p>" + project.description + "</p>");
+				
+				if (project.homepage) {
+					div.append("<p><a href='" + project.homepage + "' target='_blank'>Project webpage</a></p>");
+				}
+				$("#project-container").append(div);
+			}
+		});
+	});
+	
+	$(window).scroll(function() {
+		if ($(window).scrollTop() > 100) {
+			$("#info").addClass("visible");
+		}
+	});
 });
