a7be1008cd1f1d36ddc1c941c2dd0a6166e35e70
Tobias Belling Christensen <darkrunedk@users.noreply.github.com>
2021-12-04 23:55:58 +0100

    Updated css and js

--- js/core.js
+++ js/core.js
@@ -1,5 +1,29 @@
-var blacklist = ["gray_owl", "darkrunedk.github.io", "Monosolutions"];
+var blacklist = ["gray_owl", "darkrunedk.github.io", "Monosolutions", "darkrunedk"];
+const validTopics = ["component", "php", "javascript"];
 var githubBio = $("#githubBio");
+
+var topics = new Array();
+topics["random-stuff"] = new Array();
+
+function selectTopic(e) {
+	let topicName = e.target.innerText.toLowerCase();
+	let topic = topics[topicName];
+	
+	$("#project-container").html("");
+
+	topic.forEach(function(project) {
+		var div = $("<div id='" + project.name.toLowerCase() + "'>");
+		div.append("<h3><a href='" + project.html_url + "' target='_blank'>" + project.name.replace("-", " ") + "</a></h3>");
+		if (project.description) {
+			div.append("<p>" + project.description + "</p>");
+		}
+		
+		if (project.homepage) {
+			div.append("<p><a href='" + project.homepage + "' target='_blank'>Project webpage</a></p>");
+		}
+		$("#project-container").append(div);
+	});
+}
 
 $(document).ready(function() {
 	
@@ -25,16 +49,47 @@ $(document).ready(function() {
 		data.forEach(function(project) {
 			var blacklisted = ($.inArray(project.name, blacklist) > -1) ? true : false;
 			if (!blacklisted) {
-				var div = $("<div id='" + project.name.toLowerCase() + "'>");
-				div.append("<h3><a href='" + project.html_url + "' target='_blank'>" + project.name.replace("-", " ") + "</a></h3>");
-				div.append("<p>" + project.description + "</p>");
-				
-				if (project.homepage) {
-					div.append("<p><a href='" + project.homepage + "' target='_blank'>Project webpage</a></p>");
+
+				let topic;
+
+				let hasValidTopic = false;
+				for(let topicIndex in project.topics) {
+					if (hasValidTopic) {
+						break;
+					}
+
+					topic = project.topics[topicIndex];
+					console.log(topic);
+					if (!hasValidTopic){
+						hasValidTopic = ($.inArray(topic, validTopics) > -1) ? true : false;
+					}
 				}
-				$("#project-container").append(div);
+
+				if (hasValidTopic) {
+					if (topics[topic]) {
+						topics[topic].push(project);
+					} else {
+						topics[topic] = new Array(project);
+					}
+				} else {
+					topics["random-stuff"].push(project);
+				}
 			}
 		});
+
+		var nav = document.createElement("ul");
+
+		Object.entries(topics).forEach(([key, value]) => {
+			var navEl = document.createElement("li");
+			var link = document.createElement("a");
+			link.href = "javascript:void(0);";
+			link.onclick = selectTopic;
+			link.innerText = key;
+			navEl.appendChild(link);
+			nav.appendChild(navEl);
+		});
+
+		$("#topic-container").append(nav);
 	});
 	
 	$(window).scroll(function() {
